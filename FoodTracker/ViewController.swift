8609135dfca98cc40b798ba121d7e914645868a0
Tolhurst <ectolhurst@gmail.com>
2018-01-19 15:45:12 -0500

    current walkthrough of Apple example

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Tolhurst, Eric C. on 1/19/18.
+//  Copyright © 2018 Tolhurst, Eric C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
